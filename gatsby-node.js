72523d791f955eb95431879e310566a477cf56e9
Erik Heumrønningen <erik.heumronningen@creuna.no>
2020-09-14 22:23:36 +0200

    Create nodes for all events in D365

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,38 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-// You can delete this file if you're not using it
+const axios = require("axios");
 
-/**
- * You can uncomment the following line to verify that
- * your plugin is being loaded in your site.
- *
- * See: https://www.gatsbyjs.org/docs/creating-a-local-plugin/#developing-a-local-plugin-that-is-outside-your-project
- */
-exports.onPreInit = () => console.log("Loaded gatsby-starter-plugin")
+exports.onPreInit = () => console.log("Loaded gatsby-source-d365-events");
+
+exports.sourceNodes = async (
+  { actions, createContentDigest, createNodeId },
+  options
+) => {
+  // TODO Guards for options.endpoint, options.token, options.origin
+  const { createNode } = actions;
+
+  const request = await axios.get(
+    `${options.endpoint}/EvtMgmt/api/v2.0/events/published?emApplicationtoken=${options.token}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Origin: options.origin,
+      },
+      method: "GET",
+    }
+  );
+
+  request.data.forEach((event) =>
+    createNode({
+      ...event,
+      id: createNodeId(`Event-${event.eventId}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: "Event",
+        content: JSON.stringify(event),
+        contentDigest: createContentDigest(event),
+      },
+    })
+  );
+
+  return;
+};
