37f663ecdb93a0810d20f0acbdd86f84d613ef59
Erik Heumrønningen <erik.heumronningen@creuna.no>
2020-09-20 22:24:11 +0200

    Get sponsors and speakers and add it to the events node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,36 +1,56 @@
 const axios = require("axios");
 
-exports.onPreInit = () => console.log("Loaded gatsby-source-d365-events");
+const versionSpec = "/EvtMgmt/api/v2.0";
+const tokenParam = "?emApplicationtoken=";
+const eventsUri = `${versionSpec}/events/published${tokenParam}`;
+const sponsorshipsUri = `${versionSpec}/events/{id}/sponsorships${tokenParam}`;
+const speakersUri = `${versionSpec}/events/{id}/speakers${tokenParam}`;
+const headers = {
+  headers: {
+    "Content-Type": "application/json",
+  },
+  method: "GET",
+};
 
 exports.sourceNodes = async (
   { actions, createContentDigest, createNodeId },
   options
 ) => {
-  if (
-    options.endpoint === undefined ||
-    options.token === undefined ||
-    options.origin === undefined
-  ) {
+  if (isOptionsInitialized(options)) {
     console.error(
       "Endpoint, token and origin must be passed as options in gatsby-config.js."
     );
     return;
   }
 
+  headers.headers.Origin = options.origin;
+
+  // TODO strip trailing slash from options.endpoint
+
   const { createNode } = actions;
 
-  const request = await axios.get(
-    `${options.endpoint}/EvtMgmt/api/v2.0/events/published?emApplicationtoken=${options.token}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Origin: options.origin,
-      },
-      method: "GET",
-    }
+  const eventsResponse = await axios.get(
+    `${options.endpoint}${eventsUri}${options.token}`,
+    headers
   );
 
-  request.data.forEach((event) =>
+  const eventIds = eventsResponse.data.map((event) => event.readableEventId);
+  const speakers = await getMultipleRequests(eventIds, speakersUri, options);
+  const sponsorships = await getMultipleRequests(
+    eventIds,
+    sponsorshipsUri,
+    options
+  );
+
+  const events = eventsResponse.data.map((event) => ({
+    ...event,
+    speakers: speakers.get(event.readableEventId),
+    sponsorships: sponsorships.get(event.readableEventId),
+  }));
+
+  console.log(events);
+
+  events.forEach((event) =>
     createNode({
       ...event,
       id: createNodeId(`Event-${event.eventId}`),
@@ -46,3 +66,28 @@ exports.sourceNodes = async (
 
   return;
 };
+
+const getMultipleRequests = async (eventIds, uri, options) => {
+  return axios
+    .all(
+      eventIds.map(async (id) => {
+        const currentUri = uri.replace("{id}", id);
+        const result = await axios.get(
+          `${options.endpoint}${currentUri}${options.token}`,
+          headers
+        );
+        return [id, result.data];
+      })
+    )
+    .then(
+      axios.spread(
+        (...responses) => new Map(responses.map((response) => response))
+      )
+    )
+    .catch((errors) => console.error(errors));
+};
+
+const isOptionsInitialized = (options) =>
+  options.endpoint === undefined ||
+  options.token === undefined ||
+  options.origin === undefined;
