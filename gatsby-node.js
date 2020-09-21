f821def662ee191bd4b3fda04dd00e788d6a9edd
Erik Heumrønningen <erik.heumronningen@creuna.no>
2020-09-21 22:03:41 +0200

    Add speakers as separate node and link it to events (WIP).

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,9 @@
 const axios = require("axios");
+const { createRemoteFileNode } = require("gatsby-source-filesystem");
 
+const eventsNodeName = "D365Events";
+const speakersNodeName = "D365Speakers";
+const sponsorshipsNodeName = "D365Sponsorships";
 const versionSpec = "/EvtMgmt/api/v2.0";
 const tokenParam = "?emApplicationtoken=";
 const eventsUri = `${versionSpec}/events/published${tokenParam}`;
@@ -29,42 +33,120 @@ exports.sourceNodes = async (
 
   const { createNode } = actions;
 
-  const eventsResponse = await axios.get(
-    `${options.endpoint}${eventsUri}${options.token}`,
-    headers
-  );
+  // TODO connect sponsors to events
+  // TODO image handling for event
+  // TODO image handling for sponsor
 
-  const eventIds = eventsResponse.data.map((event) => event.readableEventId);
+  const events = await getEvents(options);
+  const eventIds = events.map((event) => event.readableEventId);
   const speakers = await getMultipleRequests(eventIds, speakersUri, options);
-  const sponsorships = await getMultipleRequests(
-    eventIds,
-    sponsorshipsUri,
-    options
-  );
 
-  const events = eventsResponse.data.map((event) => ({
+  const hydratedEvents = events.map((event) => ({
     ...event,
-    speakers: speakers.get(event.readableEventId),
-    sponsorships: sponsorships.get(event.readableEventId),
+    speakers: speakers.get(event.readableEventId).map((speaker) => speaker.id),
   }));
 
-  console.log(events);
-
-  events.forEach((event) =>
+  hydratedEvents.forEach((event) =>
     createNode({
       ...event,
-      id: createNodeId(`Event-${event.eventId}`),
+      id: event.eventId,
       parent: null,
       children: [],
       internal: {
-        type: "Event",
+        type: eventsNodeName,
         content: JSON.stringify(event),
         contentDigest: createContentDigest(event),
       },
     })
   );
 
+  const flattenedSpeakersList = flattenMap(speakers);
+  flattenedSpeakersList.forEach((speaker) =>
+    createNode({
+      ...speaker,
+      parent: null,
+      children: [],
+      internal: {
+        type: speakersNodeName,
+        content: JSON.stringify(speaker),
+        contentDigest: createContentDigest(speaker),
+      },
+    })
+  );
+
+  // const sponsorships = await getMultipleRequests(
+  //   eventIds,
+  //   sponsorshipsUri,
+  //   options
+  // );
+  // sponsorships.forEach((sponsorship) =>
+  //   createNode({
+  //     ...sponsorship,
+  //     parent: null,
+  //     children: [],
+  //     internal: {
+  //       type: sponsorshipsNodeName,
+  //       content: JSON.stringify(sponsorship),
+  //       contentDigest: createContentDigest(sponsorship),
+  //     },
+  //   })
+  // );
   return;
+};
+
+exports.onCreateNode = async (
+  { node, actions: { createNode }, store, cache, createNodeId, reporter },
+  options
+) => {
+  if (node.internal.type === speakersNodeName) {
+    try {
+      let fileNode = await createRemoteFileNode({
+        url: `${options.endpoint}${versionSpec}/${node.imageUrl}`,
+        name: node.name,
+        parentNodeId: node.id,
+        createNode,
+        createNodeId,
+        cache,
+        store,
+      });
+
+      if (fileNode) {
+        node.image___NODE = fileNode.id;
+      }
+    } catch (e) {
+      console.error("gatsby-source-d365-events ERROR:", e);
+    }
+  }
+};
+
+exports.createSchemaCustomization = ({ actions, schema }) => {
+  const { createTypes } = actions;
+  createTypes([
+    schema.buildObjectType({
+      name: eventsNodeName,
+      fields: {
+        speakers: {
+          type: `[${speakersNodeName}]`,
+          resolve: (source, args, context) => {
+            return context.nodeModel.getNodesByIds({
+              ids: source.speakers,
+              type: `${speakersNodeName}`,
+            });
+          },
+        },
+      },
+      interfaces: ["Node"],
+    }),
+  ]);
+};
+
+const getEvents = async (options) => {
+  const eventsResponse = await axios.get(
+    `${options.endpoint}${eventsUri}${options.token}`,
+    headers
+  );
+
+  return eventsResponse.data;
 };
 
 const getMultipleRequests = async (eventIds, uri, options) => {
@@ -79,13 +161,11 @@ const getMultipleRequests = async (eventIds, uri, options) => {
         return [id, result.data];
       })
     )
-    .then(
-      axios.spread(
-        (...responses) => new Map(responses.map((response) => response))
-      )
-    )
+    .then(axios.spread((...responses) => new Map(responses)))
     .catch((errors) => console.error(errors));
 };
+
+const flattenMap = (map) => [].concat.apply([], Array.from(map.values()));
 
 const isOptionsInitialized = (options) =>
   options.endpoint === undefined ||
